b066f496b261abc333dd06934d2dcaf1f3177862
aibanez <aibanez@devcenter.es>
2018-03-13 17:35:20 +0100

    resources for selects

---
+++ data/brand.js
@@ -0,0 +1,39 @@
+'use strict';
+var Mockgen = require('./mockgen.js');
+/**
+ * Operations on /brand
+ */
+module.exports = {
+    /**
+     * summary: standard response for key value lists
+     * description: 
+     * parameters: 
+     * produces: 
+     * responses: 200, default
+     * operationId: getKeyValueList
+     */
+    get: {
+        200: function (req, res, callback) {
+            /**
+             * Using mock data generator module.
+             * Replace this by actual data for the api.
+             */
+            Mockgen().responses({
+                path: '/brand',
+                operation: 'get',
+                response: '200'
+            }, callback);
+        },
+        default: function (req, res, callback) {
+            /**
+             * Using mock data generator module.
+             * Replace this by actual data for the api.
+             */
+            Mockgen().responses({
+                path: '/brand',
+                operation: 'get',
+                response: 'default'
+            }, callback);
+        }
+    }
+};
