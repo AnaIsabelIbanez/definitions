6628a69aa68b846f9be7d4e8c6c5cac1ea55f8b4
aibanez <aibanez@devcenter.es>
2018-03-22 17:27:47 +0100

    detail line get

--- tests/return/line/{id}.js
+++ tests/return/line/{id}.js
@@ -73,6 +73,55 @@ Test('/return/line/{id}', function (t) {
                     t.end();
                 });
             });
+        });/**
+         * summary: Update part of a line
+         * description: 
+         * parameters: id, data
+         * produces: 
+         * responses: 200, default
+         */
+        t.test('test updateLine get operation', function (t) {
+            Mockgen().requests({
+                path: '/return/line/{id}',
+                operation: 'get'
+            }, function (err, mock) {
+                var request;
+                t.error(err);
+                t.ok(mock);
+                t.ok(mock.request);
+                //Get the resolved path from mock request
+                //Mock request Path templates({}) are resolved using path parameters
+                request = Request(App)
+                    .get('/api' + mock.request.path);
+                if (mock.request.body) {
+                    //Send the request body
+                    request = request.send(mock.request.body);
+                } else if (mock.request.formData){
+                    //Send the request form data
+                    request = request.send(mock.request.formData);
+                    //Set the Content-Type as application/x-www-form-urlencoded
+                    request = request.set('Content-Type', 'application/x-www-form-urlencoded');
+                }
+                // If headers are present, set the headers.
+                if (mock.request.headers && mock.request.headers.length > 0) {
+                    Object.keys(mock.request.headers).forEach(function (headerName) {
+                        request = request.set(headerName, mock.request.headers[headerName]);
+                    });
+                }
+                request.end(function (err, res) {
+                    t.error(err, 'No error');
+                    t.ok(res.statusCode === 200, 'Ok response status');
+                    var Validator = require('is-my-json-valid');
+                    var validate = Validator(api.paths['/return/line/{id}']['get']['responses']['200']['schema']);
+                    var response = res.body;
+                    if (Object.keys(response).length <= 0) {
+                        response = res.text;
+                    }
+                    t.ok(validate(response), 'Valid response');
+                    t.error(validate.errors, 'No validation errors');
+                    t.end();
+                });
+            });
         });
     });
 });
